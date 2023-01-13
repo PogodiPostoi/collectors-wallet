37b6cb7837d68cee0998450433b6164adc5645d1
PogodiPostoi <boombox577@gmail.com>
2023-01-13 19:02:13 +0300

    init debts-grid and added user-block

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import Debts_grid from './pages/Debts_grid'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Debts_grid/>
     </div>
   );
 }
---
+++ src/pages/Debts_grid/index.jsx
@@ -0,0 +1,14 @@
+import './debts_grid.module.scss';
+
+const Debts_grid = () => {
+    return (
+        <div className="debts-grid">
+            <div className="debts-grid__user-block">
+                <span className="debts-grid__user-greetings">Привет, Артем!</span>
+                <span className="debts-grid__datetime">26 ноября 2022</span>
+            </div>
+        </div>
+    )
+}
+
+export default Debts_grid;
